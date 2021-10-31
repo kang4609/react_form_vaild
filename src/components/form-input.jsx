65500c5e2a3381d6d737c78cc6c722057863d512
kang4609 <kang4609@gmail.com>
2021-10-31 21:13:01 +0900

    message

---
+++ src/components/form-input.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './form-input.css';
+
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, onChange, id, errorMessage, ...inputProps } = props;
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;
